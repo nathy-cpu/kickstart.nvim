bd4f4c6cddb7809bf92ebe5e8d837e12b295ffb9
Nathnael Merihun <149013146+nathy-cpu@users.noreply.github.com>
2025-02-17 11:44:54 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- Configure tabs t be 4 spaces
 vim.opt.tabstop = 4
